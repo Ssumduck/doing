6d2e2c4eb3ce6bb175ada60606748ddfe1f43d95
Ssumduck <ktkt4744@gmail.com>
2022-09-25 16:03:55 +0900

    Update UserData.cs

--- Assets/2_Scripts/UserData.cs
+++ Assets/2_Scripts/UserData.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 
 public class UserData
 {
+    static UserData instance;
+    public static UserData Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new UserData();
+            return instance;
+        }
+    }
+
+    UserData()
+    {
+        // 초기화
+    }
+
     public Dictionary<DateTime, List<ToDo>> dicToDo;
     public Dictionary<DateTime, string> dicMemo;
 
