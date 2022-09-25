fea681f2f8e632fb125c77cee13dac6a033bafbb
Ssumduck <ktkt4744@gmail.com>
2022-09-25 14:43:43 +0900

    calendar

--- Assets/2_Scripts/CalendarUI.cs
+++ Assets/2_Scripts/CalendarUI.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CalendarUI : MonoBehaviour
 {
+    #region Inspector
     [SerializeField] GameObject containerObj;
     [SerializeField] GameObject listItemPrefab;
+    [SerializeField] Text yearTxt;
+    [SerializeField] Text monthTxt;
+    #endregion
 
-    List<GameObject> Objs = new List<GameObject>();
+    List<DayListItem> Objs = new List<DayListItem>();
+
+    [SerializeField] DateTime time;
 
     float xStartPos = 60f;
     float yStartPos = 400f;
     float xPadding = 125f;
-    float yPadding = 125f;
+    float yPadding = 115f;
 
     void Start()
     {
         Init(DateTime.Now);
+        time = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            time = time.AddMonths(1);
+
+            Init(time);
+        }
     }
 
     void Init(DateTime _time)
@@ -28,6 +46,9 @@ public class CalendarUI : MonoBehaviour
         int month = cal.GetMonth(_time);
         int days = cal.GetDaysInMonth(year, month);
         DayOfWeek startDay = cal.GetDayOfWeek(new DateTime(_time.Year, _time.Month, 1));
+
+        yearTxt.text = _time.Year.ToString() ;
+        monthTxt.text = $"{_time.Month.ToString()}월";
 
         int i = 0;
 
@@ -40,14 +61,21 @@ public class CalendarUI : MonoBehaviour
             i++;
         }
 
+        for (int j = 0; j < Objs.Count; j++)
+        {
+            Objs[j].gameObject.SetActive(false);
+        }
+
         for (int j = 0; j < days; j++, i++)
         {
+            if (Objs.Count <= j)
+            {
+                GameObject obj = Instantiate(listItemPrefab, containerObj.transform);
 
-            GameObject obj = Instantiate(listItemPrefab, containerObj.transform);
-
-            obj.GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos, yPos);
-
-            Objs.Add(obj);
+                Objs.Add(obj.GetComponent<DayListItem>());
+            }
+            Objs[j].gameObject.SetActive(true);
+            Objs[j].GetComponent<RectTransform>().anchoredPosition = new Vector2(xPos, yPos);
 
             if (i >= 6)
             {
